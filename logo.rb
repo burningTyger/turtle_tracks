5404f6ad891c874d8a5068c40ab547d8ff189340
burningTyger <b6tyger@gmail.com>
2011-10-25 20:29:00 +0200

    pretty code

--- logo.rb
+++ logo.rb
@@ -1,117 +1,16 @@
-module Logo
-	
-	class Commands
-		
-		attr_reader :pos, :canvas, :direction, :canvas_size
-		def initialize(canvas)
-			@canvas_size = canvas
-			o = @canvas_size.div(2)
-			@pos = [o,o]
-			@canvas = {}
-			@direction = [[0,-1], [1,-1], [1,0], [1,1], [0,1], [-1,1], [-1,0], [-1,-1]]
-			canvas_set		
-		end
-		
-		def call(c)
-			eval(c)
-		end
-		
-		def canvas_set
-			@canvas[@pos.dup] = true
-		end
-		
-		def turns(n)
-			raise unless n.modulo(45) == 0
-			turns = n/45
-		end
-		
-		private :canvas_set, :turns
-		
-		def fd(n)
-			n.times do
-				@pos[0] += @direction[0][0]
-				@pos[1] += @direction[0][1]
-				raise if @pos[0] > @canvas_size or @pos[1] > @canvas_size
-				canvas_set
-			end
-		end
-		
-		def rt(n)
-			@direction.push(*@direction.shift(turns(n)))
-		end
-		
-		def lt(n)
-			@direction.unshift(*@direction.pop(turns(n)))
-		end
-		
-		def bk(n)
-			rt(180)
-			fd(n)
-			lt(180)
-		end
-		
-		def repeat(n, commands)
-		  commands.cycle(n) { |c| eval(c) }
-		end
-	end
-	
-	class Interpreter
-		
-		attr_reader :prog, :attr
-		def initialize
-			@prog = []
-			@attr = ""
-		end
-		
-		def read(file)
-			File.open(file, 'r') do |f|
-				@attr = f.readline.to_i
-				f.each_line do |l|
-					l.strip!
-					next if l.empty?
-					l.downcase!
-					if l.start_with?("repeat")
-						l.sub!(/\d/, '\0,')
-						sub_str = l.scan(/\[ (.*?) \]/).join
-						sub_ary = sub_str.split
-						sub_ary = sub_ary.each_slice(2).map { |c| "'"+c.join(" ")+"'" } 
-						l.gsub!(sub_str, sub_ary.join(", "))
-						@prog << l
-					else
-						@prog << l
-					end
-				end
-			end
-		end
-		
-		def run
-			app = Commands.new(@attr)
-			@prog.each {|c| app.call(c) }
-			app.canvas
-		end
-	end	
+# see logo_lib.rb for more information
+# 
+# to run
+# % ruby main.rb [input_file output_file]
+# if no argument is give the complex.logo file will be used
+require_relative 'logo_lib'
+include Logo
+input  = ARGV[0] || "complex.logo"
+output = ARGV[1] || "complex_out.txt"
+i = Interpreter.new
+i.read(input)
+result = i.run
 
-	class Output
-		
-		def initialize
-			@str = ""
-		end
-	
-		def write(file)
-			File.open(file, 'w') {|f| f.write(@str) }
-		end
-		
-		def out(size, positions)
-			size.times do |y|
-				size.times do |x|
-					if positions[[x,y]]
-						@str << "X "
-					else
-						@str << ". "
-					end
-				end
-				@str.rstrip! << "\n"
-			end
-		end
-	end
-end
+o = Output.new
+o.out(i.attr, result)
+o.write(output)
--- test.rb
+++ test.rb
@@ -1,78 +1,78 @@
 require 'minitest/autorun'
-require_relative 'logo'
+require_relative 'logo_lib'
 
 include Logo
 
 class CommandsTest < MiniTest::Unit::TestCase
-	def setup
-		@l = Commands.new(5)
-	end
-	
+  def setup
+    @l = Commands.new(5)
+  end
+  
   def test_canvas_set
     assert_equal @l.pos, [2,2]
   end
   
   def test_rt
-  	@l.rt(90)
+    @l.rt(90)
     assert_equal @l.direction[0], [1,0]
   end
 
   def test_lt
-  	@l.lt(90)
+    @l.lt(90)
     assert_equal @l.direction[0], [-1,0]
   end
 
   def test_fd
-  	@l.fd(2)
+    @l.fd(2)
     assert_equal @l.pos, [2,0]
   end
 
   def test_bk
-  	dir = @l.direction.dup
-  	@l.bk(2)
+    dir = @l.direction.dup
+    @l.bk(2)
     assert_equal @l.pos, [2,4]
     assert_equal @l.direction, dir
   end
 
   def test_repeat
-  	@l.repeat(2, ["lt 90", "fd 2"])
+    @l.repeat(2, ["lt 90", "fd 2"])
     assert_equal @l.pos, [0,4]
   end
   
   def test_rt_fd
-  	@l.bk(2) #3,5
-  	@l.rt(45)
-  	@l.fd(1) #4,4
-  	@l.lt(90)
-  	@l.fd(2) #2,2
+    @l.bk(2) #3,5
+    @l.rt(45)
+    @l.fd(1) #4,4
+    @l.lt(90)
+    @l.fd(2) #2,2
     assert_equal @l.pos, [1,1]
   end
   
   def test_canvas
-  	@l.bk(2) #2
-  	@l.rt(45)
-  	@l.fd(1) #3
-  	@l.lt(90)
-  	@l.fd(2) #5
+    @l.bk(2) #2
+    @l.rt(45)
+    @l.fd(1) #3
+    @l.lt(90)
+    @l.fd(2) #5
     assert_equal @l.canvas.keys, [[2,2], [2,3], [2,4], [3,3], [1,1]]
   end
 end
 
 class InterpreterTest < MiniTest::Unit::TestCase
-	def setup
-		@i = Interpreter.new
-	end
-	
-	def test_read
-		@i.read("simple.logo")
-		assert_equal @i.attr, 61
-		assert_equal @i.prog.size, 7
-		assert_equal @i.prog[0], "rt 135"
-		assert_equal @i.prog[2], "repeat 2, [ 'rt 90', 'fd 15' ]"
-	end
-	
-	def test_run
-		@i.read("simple.logo")
-		@i.run
-	end
+  def setup
+    @i = Interpreter.new
+  end
+  
+  def test_read
+    @i.read("simple.logo")
+    assert_equal @i.attr, 61
+    assert_equal @i.prog.size, 7
+    assert_equal @i.prog[0], "rt 135"
+    assert_equal @i.prog[2], "repeat 2, [ 'rt 90', 'fd 15' ]"
+  end
+  
+  def test_run
+    @i.read("simple.logo")
+    @i.run
+  end
 end
